0e91dddfd3da357093df72f197a1151dd97d5674
Ian <ianlucasott@gmail.com>
2026-05-25 23:29:51 -0300

    add posible resolucion

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,3 +1,16 @@
 # TODO: Locust load test
 # from locust import HttpUser, task
 # Hit /health and /api/nodes endpoints
+
+from locust import HttpUser, task, between
+
+class ApiUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task(3)
+    def health(self):
+        self.client.get("/health")
+
+    @task(1)
+    def get_nodes(self):
+        self.client.get("/api/nodes")
